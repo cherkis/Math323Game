3772659a5a91ef299507304554d14f9706ee818f
rafaelgrenier <rafaelmgren@arizona.edu>
2026-04-13 19:00:52 -0700

    PredicateLogic in central.lean development

--- docs/central.lean
+++ docs/central.lean
@@ -1,8 +1,10 @@
---import Mathlib.Tactic
+import Mathlib.Tactic
 
 /-
  # Central Problems for Each Stage
-Each stage should have about 2 core theorems about the concept
+Each stage should have about 2 core theorems about the concept.
+This document is meant to help scaffold for the stages, so hint
+infrastructure and the like does not yet need to be consider
 -/
 
 namespace LeanAndLogic -- Stage 1
@@ -23,15 +25,52 @@ end LeanAndLogic
 
 namespace PredicateLogic -- Stage 2
 /--
-  Human Language Proof Goes Here
+  We prove the statements comprising the conjunction separately.
+
+  For the forward direction, we are required to use classical logic.
+  The contrapositive to the forwards direction
+  is "¬ (∃ x, ¬ P x) → ∀ x,  P x". Suppose that there does not exist
+  any x such that P x is false, and let x be an arbitrary term of type X.
+  Suppose P x is false. Then we have shown that there DOES exist an x
+  such that P x is false, so we have contradicted our previous assumption.
+  Therefore our supposition that "P x is false" is itself false, i.e.
+  P x is true.
+
+  For the backward direction, suppose that there exists an x such that
+  P x is false. Then clearly it is false that for all x, P x is true,
+  since we have a counterexample. In more detail, if we assume that
+  it is true that "∀ x, P x", then we have that P x is true, but we had
+  already assumed that P x is false. Since P x cannot be both true and false,
+  the claim "∀ x, P x" must be false.
+
 -/
-theorem prob1 : True := sorry
+theorem quantifier_negation {X : Type} (P : X → Prop) :
+  (¬ (∀ x, P x) ↔ ∃ x, ¬ P x):= sorry
 
 
 /--
-  Human Language Proof Goes Here
+  First, we prove the forward direction, so we suppose that
+  (∃ x, P x) → Q. In other words, if we can find a single
+  element x such that P x is true, then Q is also true. Now we
+  want to show that for all x, if P x is true, then Q is true.
+  Let x be an arbitrary element such that P x is true. Then by our
+  initial supposition, Q is true, and we are done.
+
+  Now we prove the backwards direction, so we suppose that
+  ∀ x, P x → Q, and we want to show that (∃ x, P x) → Q. So
+  we further assume that there exists some x such that P x is true.
+  By initial assumption, we have that Q is true, and again, we are done.
 -/
-theorem prob2 : True := sorry
+theorem exists_imp_iff_forall_imp {X : Type} (P : X → Prop) (Q : Prop) :
+  ((∃ x, P x) → Q) ↔ (∀ x, P x → Q) := by
+  constructor
+  · intro h x hx
+    apply h
+    exists x
+  · rintro h ⟨x, hx⟩
+    apply h x
+    exact hx
+
 
 end PredicateLogic
 
@@ -105,16 +144,8 @@ namespace Cardinality -- Stage 7
 /--
   Human Language Proof Goes Here
 -/
-theorem cantor {X : Type} (f : X → X → Bool) :
-  ∃ g : X → Bool, ∀ x : X, f x ≠ g := by
-  exists (fun x => not $ f x x)
-  intro x h
-  have : ∀ b : Bool, ¬(not b = b) := fun b => Bool.not_not_eq.mpr rfl
-  apply this $ f x x
-  rewrite (occs := .pos [2]) [h]
-  rfl
-  done
-
+theorem Cantor (X : Type) (f : X → Set X) :
+  ¬ Function.Surjective f := sorry
 
 /--
   Human Language Proof Goes Here
