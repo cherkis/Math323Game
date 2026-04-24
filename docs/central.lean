3a339a6de0b93b808494ba70f08df769ee4d63f1
rafaelgrenier <rafaelmgren@arizona.edu>
2026-04-23 19:49:47 -0700

    PredicateLogic in central.lean redux

--- docs/central.lean
+++ docs/central.lean
@@ -71,26 +71,57 @@ theorem prob11_2: P → ¬¬P := by
   contradiction
 
 
-/--
-  Human Language Proof Goes Here
--/
-
 end LeanAndLogic
 
 
 
 namespace PredicateLogic -- Stage 2
 /--
-  Human Language Proof Goes Here
+  We prove the statements comprising the conjunction separately.
+
+  For the forward direction, we are required to use classical logic.
+  The contrapositive to the forwards direction
+  is "¬ (∃ x, ¬ P x) → ∀ x,  P x". Suppose that there does not exist
+  any x such that P x is false, and let x be an arbitrary term of type X.
+  Suppose P x is false. Then we have shown that there DOES exist an x
+  such that P x is false, so we have contradicted our previous assumption.
+  Therefore our supposition that "P x is false" is itself false, i.e.
+  P x is true.
+
+  For the backward direction, suppose that there exists an x such that
+  P x is false. Then clearly it is false that for all x, P x is true,
+  since we have a counterexample. In more detail, if we assume that
+  it is true that "∀ x, P x", then we have that P x is true, but we had
+  already assumed that P x is false. Since P x cannot be both true and false,
+  the claim "∀ x, P x" must be false.
+
 -/
-theorem prob1 : True := sorry
+theorem quantifier_negation {X : Type} (P : X → Prop) :
+  (¬ (∀ x, P x) ↔ ∃ x, ¬ P x):= sorry
 
 
 /--
-  Human Language Proof Goes Here
--/
-theorem prob2 : True := sorry
+  First, we prove the forward direction, so we suppose that
+  (∃ x, P x) → Q. In other words, if we can find a single
+  element x such that P x is true, then Q is also true. Now we
+  want to show that for all x, if P x is true, then Q is true.
+  Let x be an arbitrary element such that P x is true. Then by our
+  initial supposition, Q is true, and we are done.
 
+  Now we prove the backwards direction, so we suppose that
+  ∀ x, P x → Q, and we want to show that (∃ x, P x) → Q. So
+  we further assume that there exists some x such that P x is true.
+  By initial assumption, we have that Q is true, and again, we are done.
+-/
+theorem exists_imp_iff_forall_imp {X : Type} (P : X → Prop) (Q : Prop) :
+  ((∃ x, P x) → Q) ↔ (∀ x, P x → Q) := by
+  constructor
+  · intro h x hx
+    apply h
+    exists x
+  · rintro h ⟨x, hx⟩
+    apply h x
+    exact hx
 end PredicateLogic
 
 
